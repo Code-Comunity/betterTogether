a949150f2f226cc8e9b42404a32f6023dcfec889
Patrick Rocha <ptkdev@outlook.com>
2020-08-30 14:42:21 -0300

    Api fake, uso do map, tela de login de admin e cliente criada, dashboard incompleto

--- src/pages/clientes/Login/index.js
+++ src/pages/clientes/Login/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../../assets/logowhite.svg';
+import './loginclient.css';
 
-export default function componentName() {
+export default function LoginClient() {
   return (
     <>
-    
+    <body>
+    <div className="loginarea">
+
+      <div className="boxLogin">
+      <img src={Logo} alt="betterTogether"/>
+      <h3 style={{color: "#FFF",}}>Login Area Cliente</h3>
+      
+        <div className="inputGroup">
+
+          <div className="inputWIcon">
+            <h3>E</h3>
+            <input type="text" name="email"/>
+          </div>
+
+          <div className="inputWIcon">
+            <h3>S</h3>
+            <input type="password" name="senha"/>
+          </div>
+        </div>
+        <Link style={{color: "#FFF"}} to="/"><h3>Sou novo aqui</h3></Link>
+        <button className="BTNLogin">Entrar</button>
+      </div>
+       
+      </div>
+    </body>
     </>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,21 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 //Rotas de admin
 import LoginAdmin from './pages/admin/Login';
+import Dashboard from './pages/admin/Dashboard';
 
 //Rotas de usuario comum
 import Home from './pages/clientes/Home'
+import LoginClient from './pages/clientes/Login';
 
 export default function Rotas() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route path="/" exact component={Home} />
         <Route path="/loginadmin" component={LoginAdmin} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/login" component={LoginClient} />
+        
       </Switch>
     </BrowserRouter>
   );
