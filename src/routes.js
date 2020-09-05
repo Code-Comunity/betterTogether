f5e04d9bc8f1acb494221997ef33c6a9bd12c2f2
SaraivaVitor <vitorsaraivapereira1@gmail.com>
2020-09-05 15:57:31 -0300

    Criei um component Modal para o carrinho

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ import Clientes from './pages/admin/Clientes';
 //Rotas de usuario comum
 import Home from './pages/clientes/Home';
 import Produto from './pages/clientes/Produto';
-import Contato from './pages/clientes/Contato';
+import Sobre from './pages/clientes/Sobre';
 import LoginClient from './pages/clientes/Login';
 
 export default function Rotas() {
@@ -20,11 +20,11 @@ export default function Rotas() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/produto" component={Produto} />
-        <Route path="/contato" component={Contato} />
+        <Route path="/sobre" component={Sobre} />
+        <Route path="/login" component={LoginClient} />
 
         <Route path="/loginadmin" component={LoginAdmin} />       
         <Route path="/dashboard" component={Dashboard} />
-        <Route path="/login" component={LoginClient} />
         <Route path="/addprodutos" component={AddProdutos} />
         <Route path="/clientes" component={Clientes} />
       </Switch>
