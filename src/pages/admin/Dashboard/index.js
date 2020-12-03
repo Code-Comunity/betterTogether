1cc852aa9e19e08a82f5691213a3e433cb93eeae
Vitor Pereira <vitorsaraivapereira1@gmail.com>
2020-12-03 10:56:35 -0300

    Finalização dos cards dinamicos

--- src/pages/admin/Dashboard/index.js
+++ src/pages/admin/Dashboard/index.js
@@ -1,6 +1,10 @@
 import React, { useEffect,useState } from 'react';
 import { BiFace,BiHomeAlt,BiDollarCircle,BiMap,BiCar, BiPackage } from "react-icons/bi";
-import { IoMdCalendar,IoIosExit,IoMdCash, IoMdClock,IoMdBarcode,IoMdShareAlt } from "react-icons/io";
+import { IoMdCalendar,IoIosExit,IoMdCash, IoMdBarcode,IoMdShareAlt } from "react-icons/io";
+import { FaBoxOpen } from "react-icons/fa";
+import { GrStatusGood } from "react-icons/gr";
+import { BsCalendarFill } from "react-icons/bs";
+import { AiFillDollarCircle } from "react-icons/ai";
 import {Link} from 'react-router-dom';
 import './dashboard.css'
 import Logo from '../../../assets/logored.svg';
@@ -21,16 +25,18 @@ export default function Dashboard() {
     
     async function getApi(){
       try{
-        const req = await api.get(`/pagarme-todastransacoes`)
-        console.log(req)
-        return setPedidos(req);
+        const req = await api.get('/pagarme-todastransacoes')
+       
+        setPedidos(req.data.map(pedidos=>pedidos));
+        
+        return
       }catch(error){
         console.log(error)
       }
     }
     getApi();
   },[])
-  console.log(pedidos.data)  
+  console.log(pedidos)
 
 
   useEffect(()=>{
@@ -57,12 +63,12 @@ export default function Dashboard() {
     }
 
     async function TodasTransactions(){
-      try{
-        const {data} = await api.get('/pagarme-todastransacoes')
-        setPedidos(data.transactions);
-      }catch(error){
-        console.log(error)
-      }
+      // try{
+      //   const {data} = await api.get('/pagarme-todastransacoes')
+      //   setPedidos(data.transactions);
+      // }catch(error){
+      //   console.log(error)
+      // }
     }
     async function BuscaRecebiveis(){
       try {
@@ -118,7 +124,7 @@ export default function Dashboard() {
           <div className="direta">
             <div className="perfilDash">
               <div className="circleDash"></div>
-              <h2>{usuarioLogado.nome}</h2>
+              <h2>{/*usuarioLogado.nome*/}</h2>
               <h3> <IoIosExit className="exitbtn" size="23px" color="#820E0E" onClick={()=>Deslogar()} /> </h3>
             </div>
           </div>
@@ -134,48 +140,38 @@ export default function Dashboard() {
             <div className="ultimosPedidos">
               <h1 style={{color: '#820E0E', fontSize: 20}}>Ultimos pedidos</h1>
 
-            <div className="pedidoslista">
-
-              {/** USAR a variavel de estado pedidos em vez do data, ficando assim pedidos.map()
-               * mas atentar-se ao objeto retornado, alguns possuem objetos dentro de objetos
-               * e arrays dentro do objeto, portanto possivelmente precise de um map dentro de um map...
-               */}
-
-           
-
-              { data.map(e=>{
-                //Aqui chamaremos na api, os ultimos pedidos
+            <div className="pedidoslista" style={{overflow:"auto", maxHeight:"450px"}} >
+              { pedidos.map(e=>{
                 return(
                 <div key={e.id} className="itemLista">
                   <div className="top">
-                    <button onClick={()=>Estornar(e.id)}>Realizar Estorno</button>
                     <div className="nome">
-                    <h2 style={{marginRight:10}}>{e.thumb}</h2>
-                    <h3 style={{color: '#820E0E'}}>{e.nome}</h3>
+                    <h2 style={{marginRight:10}}>{/*e.thumb*/}</h2>
+                    <h3 style={{color: '#820E0E'}}>{ e.customer.name }</h3>
                     </div>
                   </div>
 
                   <div className="bottom">
                   <div className="infos">
                       <div className="infoitems">
-                          <span><BiMap />{e.local}</span>
-                          <span><BiPackage />{e.status}</span>
+                          <span><BiMap />{e.shipping.city}</span>
+                          <span><GrStatusGood />{e.status}</span>
                       </div>
                   <div className="infoitems">
-                          <span><IoMdClock />{e.hora}</span>
-                          <span><IoMdBarcode />{e.codigo}</span>
+                          <span><FaBoxOpen />{e.items.map(e=>(e.title))}</span>
+                          <span><IoMdBarcode />{e.id}</span>
                       </div>
                     <div className="infoitems">
-                        <span><IoMdShareAlt />{e.frete}</span>
-                        <span><BiCar />{e.transportadora}</span>
+                        <span><BsCalendarFill />{e.shipping.delivery_date}</span>
+                        <span><AiFillDollarCircle />{e.payment_method}</span>
                     </div>
                       </div>
                     <div className="total">
                           <IoMdCash size="25px" color="green"/>
-                          <h3>{e.valorTotal}</h3>
+                          <h3>{e.items.map(e=>(e.unit_price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})))}</h3>
                     </div>
                   </div>
-
+                  <button style={{color: "#820E0E", marginTop: "15px"}} onClick={()=>Estornar(e.id)}>Realizar Estorno</button>    
                 </div>
                 )
                 }) }
